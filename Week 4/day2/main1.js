60761af11d9dfd0eabac56e9cb99e3558c15e9c1
lyran10 <liranramekar629@gmail.com>
2022-04-07 20:50:33 +0300

    update

--- Week 4/day2/main1.js
+++ Week 4/day2/main1.js
@@ -1,67 +1,39 @@
  //Exercise 1
 
-//  let randomNum = (n) => {
-//    let arr = []
-//     let num = Math.floor(Math.random()*n)
-//     console.log(`Random number ${num}`)
-//     for(i=0;i<= num;i++){
-//       if(i%2==0){
-//         arr.push(i)
-//       }
-//     }return arr
-//  }
-//  console.log(randomNum(50))
+ let randomNum = (n) => {
+   let arr = []
+    let num = Math.floor(Math.random()*n)
+    console.log(`Random number ${num}`)
+    for(i=0;i<= num;i++){
+      if(i%2==0){
+        arr.push(i)
+      }
+    }return arr
+ }
+ console.log(randomNum(50))
 
 //Exercise 2
 
-// let capAndLow = (str) =>{
-// let str1 = str.split("")
-// let arr = []
-//   let upper = str1.map((x,i) => {
-//     if(i%2==0){
-//       return x.toUpperCase()
-//     }else{
-//       return x.toLowerCase()
-//     }
-//   })
-// let lower = str1.map((x,i) => {
-//   if(i%2!=0){
-//     return x.toUpperCase()
-//   }else{
-//     return x.toLowerCase()
-//   }
-// })
-// arr.push(upper.join(""),lower.join(""))
-// return arr
-// }
-
-// console.log(capAndLow("asfdasfas"))
-
-
-// let stock = { 
-//   "banana": 6, 
-//   "apple": 0,
-//   "pear": 12,
-//   "orange": 32,
-//   "blueberry":1
-// }  
-
-// let prices = {    
-//   "banana": 4, 
-//   "apple": 2, 
-//   "pear": 1,
-//   "orange": 1.5,
-//   "blueberry":10
-// } 
-
-// let shop = ["banana","apple","orange"]
-// let totalcost = 0
-// for (const i of shop) {
-//   totalcost =+ stock[i]
-// }
-
-// console.log(totalcost)
-
-
-let playTheGame = ()
+let capAndLow = (str) =>{
+let str1 = str.split("")
+let arr = []
+  let upper = str1.map((x,i) => {
+    if(i%2==0){
+      return x.toUpperCase()
+    }else{
+      return x.toLowerCase()
+    }
+  })
+let lower = str1.map((x,i) => {
+  if(i%2!=0){
+    return x.toUpperCase()
+  }else{
+    return x.toLowerCase()
+  }
+})
+arr.push(upper.join(""),lower.join(""))
+return arr
 }
+
+
+console.log(capAndLow("asfdasfas"))
