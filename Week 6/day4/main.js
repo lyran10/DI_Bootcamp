e09aa06456cabe5bbc21352b5ed71f46970d43fa
lyran10 <liranramekar629@gmail.com>
2022-04-27 20:47:27 +0300

    6 week days4

---
+++ Week 6/day4/main.js
@@ -0,0 +1,111 @@
+// let menu = [
+//   {
+//     type : "starter",
+//     name : "Houmous with Pita"
+//   },
+//   {
+//     type : "starter",
+//     name : "Vegetable Soup with Houmous peas"
+//   },
+//   {
+//     type : "dessert",
+//     name : "Chocolate Cake"
+//   }
+// ]
+// Exercise 1
+
+//Create a one line function (ie. an arrow function) that receives two numbers as parameters and returns the sum.
+
+// let add = (a,b) => a+b
+
+// console.log(add(5,4))
+
+//----------------------------------------------------------------------------------
+
+
+//Exercise 2
+//Create a function that receives a weight in kilograms and returns it in grams. (Hint: 1 kg is 1000gr)
+
+// (function(){
+//   var gram = kg*1000
+//   console.log(gram)
+// }())
+
+
+// let convert1 = (function(kg){
+//   console.log(kg*1000)
+// })(2)
+
+// declaration functions can be hoisted and expressions function not
+
+// let convert2 =((kg) =>{
+//   console.log(kg*1000)
+// })(20) 
+//----------------------------------------------------------------------------------
+
+//Exercise 3
+
+//Create a self invoking function that takes 4 arguments: number of children, partner’s name, geographic location, job title.
+
+// let data = (function(num,name,location,title){
+//   console.log(`You will be a ${title} in ${location}, and married to ${name} with ${num} kids.`
+//   )
+// })(2,"vidhi","mumbai","web developer")
+
+//Exercise 4
+
+let signIn = ((name,pic) => {
+    let container = document.querySelector("#container")
+    let div = document.createElement("div")
+    container.appendChild(div)
+    div.classList.add("d-flex","gap-2")
+    let p = document.createElement("p")
+    let img = document.createElement("img")
+    div.appendChild(p)
+    p.classList.add("mt-2")
+    div.appendChild(img).src = pic
+    img.style.width = "40px"
+    img.style.height = "40px"
+    img.style.borderRadius = "100px"
+    p.innerHTML = name
+})("liran","liran.jpg")
+
+// Exercise 5
+
+function makeJuice(size){
+    let ingrediants = []
+
+    function addIngrediants(ingrediant1,ingrediant2,ingrediant3){
+            ingrediants.push(ingrediant1,ingrediant2,ingrediant3)
+            console.log(`The client wants a ${size} juice, containing ${ingrediant1}, ${ingrediant2}, ${ingrediant3}`)
+
+           function displayJuice(){
+            ingrediants.forEach(ele,index,arr => console.log(console.log(`The client wants a ${size} juice, containing ${arr[index]}, ${arr[index]}, ${arr[index]}`)))
+           }
+           displayJuice()
+    }
+    addIngrediants("Beet","Carrot","Lemon")
+    addIngrediants("Apple","Banana","lemon")
+}
+makeJuice("medium")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
