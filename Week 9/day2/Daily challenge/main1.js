2448aff17138516b94a1272c3db28c77e5fe3b26
lyran10 <liranramekar629@gmail.com>
2022-05-24 22:44:21 +0300

    week9 day2

--- Week 9/day2/Daily challenge/main1.js
+++ Week 9/day2/Daily challenge/main1.js
@@ -1,4 +1,4 @@
-// Daily challenge
+//Daily challenge
 
 // let makeAllCaps = (arr) => {
 //   return new Promise((resolve,reject) => {
@@ -34,94 +34,95 @@
 
 //----------------------------------------------------------------------------------
 
-let morse = `{
-  "0": "-----",
-  "1": ".----",
-  "2": "..---",
-  "3": "...--",
-  "4": "....-",
-  "5": ".....",
-  "6": "-....",
-  "7": "--...",
-  "8": "---..",
-  "9": "----.",
-  "a": ".-",
-  "b": "-...",
-  "c": "-.-.",
-  "d": "-..",
-  "e": ".",
-  "f": "..-.",
-  "g": "--.",
-  "h": "....",
-  "i": "..",
-  "j": ".---",
-  "k": "-.-",
-  "l": ".-..",
-  "m": "--",
-  "n": "-.",
-  "o": "---",
-  "p": ".--.",
-  "q": "--.-",
-  "r": ".-.",
-  "s": "...",
-  "t": "-",
-  "u": "..-",
-  "v": "...-",
-  "w": ".--",
-  "x": "-..-",
-  "y": "-.--",
-  "z": "--..",
-  ".": ".-.-.-",
-  ",": "--..--",
-  "?": "..--..",
-  "!": "-.-.--",
-  "-": "-....-",
-  "/": "-..-.",
-  "@": ".--.-.",
-  "(": "-.--.",
-  ")": "-.--.-"
-}`
-let toJs = () => {
-  let json = JSON.parse(morse)
-  console.log(json[0])
-  return new Promise((resolve,reject) => {
-    if (json.length === 0){
-      reject("yes")
-    }else{
-      resolve(json)
-    }
-  })
-}
+// second daily chelleng
 
-let toMorse = (keys) => {
-  return new Promise((resolve,reject) => {
-  let user = prompt("Enter a phrase")
-  let splitWord = user.split("").filter(ele => ele != " ")
-  let objectKeys = Object.keys(keys)
-  let count = 0
- while(count != splitWord.length){
-    let compare = objectKeys.map((ele) => {ele === splitWord[count]})
-    if(compare === false){
-      break
-    }
-    count++
-   }
-    if(count === splitWord.length){
-      let mapped = splitWord.map(ele => objectKeys.indexOf(ele) != -1?ele =  keys[ele]:ele)
-      resolve(mapped)
-    }else{
-      reject("not same")
-    }
-})
-}
+// let morse = `{
+//   "0": "-----",
+//   "1": ".----",
+//   "2": "..---",
+//   "3": "...--",
+//   "4": "....-",
+//   "5": ".....",
+//   "6": "-....",
+//   "7": "--...",
+//   "8": "---..",
+//   "9": "----.",
+//   "a": ".-",
+//   "b": "-...",
+//   "c": "-.-.",
+//   "d": "-..",
+//   "e": ".",
+//   "f": "..-.",
+//   "g": "--.",
+//   "h": "....",
+//   "i": "..",
+//   "j": ".---",
+//   "k": "-.-",
+//   "l": ".-..",
+//   "m": "--",
+//   "n": "-.",
+//   "o": "---",
+//   "p": ".--.",
+//   "q": "--.-",
+//   "r": ".-.",
+//   "s": "...",
+//   "t": "-",
+//   "u": "..-",
+//   "v": "...-",
+//   "w": ".--",
+//   "x": "-..-",
+//   "y": "-.--",
+//   "z": "--..",
+//   ".": ".-.-.-",
+//   ",": "--..--",
+//   "?": "..--..",
+//   "!": "-.-.--",
+//   "-": "-....-",
+//   "/": "-..-.",
+//   "@": ".--.-.",
+//   "(": "-.--.",
+//   ")": "-.--.-"
+// }`
+// let toJs = () => {
+//   let json = JSON.parse(morse)
+//   return new Promise((resolve,reject) => {
+//     if (json.length === 0){
+//       reject("error")
+//     }else{
+//       resolve(json)
+//     }
+//   })
+// }
 
-toJs().then((result) => {
-  return result
-}).then((result1) => {
-  console.log(toMorse(result1))
-}).catch((msg) => {
-  console.log(msg)
-})
+// let toMorse = (keys) => {
+//   return new Promise((resolve,reject) => {
+//   let user = prompt("Enter a phrase")
+//   let splitWord = user.split("").filter(ele => ele != " ")
+//   let objectKeys = Object.keys(keys)
+//   let count = 0
+//  while(count != splitWord.length){
+//     let compare = objectKeys.some((ele) => ele === splitWord[count])
+//     count++
+//     if(!compare){
+//       break
+//     }
+//    }
+//     if(count === splitWord.length){
+//       let morsed = splitWord.map(ele => objectKeys.indexOf(ele) != -1?ele =  keys[ele]:ele)
+//       return resolve(morsed)
+//     }else{
+//       return reject("not same")
+//     }
+// })
+// }
+
+// toJs().then((result) => {
+//   return result
+// }).then((result) => {
+//   console.log(toMorse(result))
+// }).catch((msg) => {
+//   return msg
+// })
 
 
 
