d8860dea71c9f547e2ef81f42c76b543e5fdc9a3
lyran10 <liranramekar629@gmail.com>
2022-05-24 16:09:25 +0300

    week9 day2

---
+++ Week 9/day2/main1.js
@@ -0,0 +1,49 @@
+//Exercise 1 comparison
+
+// let compare = (num) => new Promise((resolve,reject) => {
+//   if(num < 10){
+//     resolve("Greater")
+//   }else{
+//     reject("smaller")
+//   }
+
+// })
+
+// compare(5)
+// .then( (msg) => {
+//   console.log(`10 is ${msg}`)
+// })
+// .catch((msg) => {
+//   console.log(`10 is ${msg}`)
+// })
+
+//Exercise 2 Promises
+
+// Promise.resolve("success").then((msg) => {
+//  setTimeout(() =>{
+//    console.log(msg)
+//  },4000)
+// })
+
+
+// //Exercise 3
+
+// Promise.resolve(3).then((value)=> {
+//   console.log(value)
+// })
+
+// Promise.reject("Boo").catch((value)=> {
+//   console.log(value)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
