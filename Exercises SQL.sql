eed5fa0ab583b06b5878b1ce49b70f65921c183e
lyran10 <101665844+lyran10@users.noreply.github.com>
2022-05-30 20:07:06 +0300

    Add files via upload

---
+++ Exercises SQL.sql
@@ -0,0 +1,11 @@
+-- Exercise 1
+
+SELECT * FROM Items;
+
+SELECT * FROM Items WHERE price > 80;
+
+SELECT * FROM Customers WHERE last_name = 'smith';
+
+SELECT * FROM Customers WHERE last_name = 'Jones';
+
+SELECT * FROM Customers WHERE last_name != 'Scott';
